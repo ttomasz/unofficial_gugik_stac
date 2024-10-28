afbf28b6821497bf06573c235df18b04559332a5
Tomasz Taraś <tomasztaras@outlook.com>
2024-10-28 15:25:06 +0000

    configured default logger settings

--- ggkstac/src/ggkstac/log.py
+++ ggkstac/src/ggkstac/log.py
@@ -1,3 +1,13 @@
 import logging
+import sys
 
+logging_level = logging.INFO
+stdoutHandler = logging.StreamHandler(stream=sys.stdout)
+fmt = logging.Formatter(
+    "%(name)s: %(asctime)s | %(levelname)s | %(filename)s:%(lineno)s | %(process)d >>> %(message)s",
+)
+stdoutHandler.setFormatter(fmt)
+stdoutHandler.setLevel(logging_level)
 logger = logging.getLogger("ggkstac")
+logger.setLevel(logging_level)
+logger.addHandler(stdoutHandler)
