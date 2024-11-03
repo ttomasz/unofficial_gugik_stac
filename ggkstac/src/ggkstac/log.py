fcfb9d5e7907b01bf2ffbacbcd847d22b5fd1466
Tomasz Taraś <tomasztaras@outlook.com>
2024-11-03 22:57:33 +0000

    changed logging format and added method for updating level

--- ggkstac/src/ggkstac/log.py
+++ ggkstac/src/ggkstac/log.py
@@ -4,10 +4,16 @@ import sys
 logging_level = logging.INFO
 stdoutHandler = logging.StreamHandler(stream=sys.stdout)
 fmt = logging.Formatter(
-    "%(name)s: %(asctime)s | %(levelname)s | %(filename)s:%(lineno)s | %(process)d >>> %(message)s",
+    "%(asctime)s [%(name)s][%(levelname)s] - %(message)s",
 )
 stdoutHandler.setFormatter(fmt)
 stdoutHandler.setLevel(logging_level)
 logger = logging.getLogger("ggkstac")
 logger.setLevel(logging_level)
 logger.addHandler(stdoutHandler)
+
+def set_logging_level(level: str) -> None:
+    global logging_level, stdoutHandler, logger
+    logging_level = logging.getLevelNamesMapping()[level]
+    stdoutHandler.setLevel(logging_level)
+    logger.setLevel(logging_level)
